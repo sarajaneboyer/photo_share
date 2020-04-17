8edbdbdea977af50179f79718b02ecb440ba4a94
Sara Boyer <sara.jane.boyer@gmail.com>
2020-04-17 16:15:24 -0600

    likes background and color changed

--- client/src/components/ItemView.js
+++ client/src/components/ItemView.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { FaThumbsUp } from 'react-icons/fa';
 import { Card, Image, } from 'semantic-ui-react';
+import '../Home.css';   
 
 const ItemView = (props) => {
 
