26bc841beed324422667e0703bc15862b38b38a0
Sara Boyer <sara.jane.boyer@gmail.com>
2020-04-17 15:43:42 -0600

    building custom card

--- client/src/components/ItemView.js
+++ client/src/components/ItemView.js
@@ -3,18 +3,49 @@ import { Card, Image, } from 'semantic-ui-react';
 
 const ItemView = (props) => {
 
-  return (
-    <>
-      <Card color="pink">
-        <Card.Content>
-          <Image size="medium" src={props.image} />
-          <Card.Header>{props.name}</Card.Header>
-          <Card.Meta>{props.likes} likes</Card.Meta>
-          <Card.Description>{props.description}</Card.Description>
-        </Card.Content>
-      </Card>
-    </>
+  // return (
+  //   <>
+  //     <Card color="pink">
+  //       <Card.Content>
+  //         <Image size="medium" src={props.image} />
+  //         <Card.Header>{props.name}</Card.Header>
+  //         <Card.Meta>{props.likes} likes</Card.Meta>
+  //         <Card.Description>{props.description}</Card.Description>
+  //       </Card.Content>
+  //     </Card>
+  //   </>
+  // )
+
+  return(
+    <div style={styles.postCard}>
+      <div style={styles.image}><img src={`${props.image}`}></img></div>
+      <div style={styles.name}>{props.name}</div>
+      <div style={styles.likes}>{props.likes} likes</div>
+      <div style={styles.description}>{props.description}</div>
+    </div>
   )
-}
+};
+
+const styles = {
+  postCard: {
+    borderRadius: '5px',
+    backgroundColor: 'pink',
+    color: 'white',
+    margin: '15px',
+  },
+  image: {
+
+  },
+  name: {
+
+  },
+  likes: {
+    color: 'grey',
+  },
+  description: {
+
+  },
+};
 
 export default ItemView;
+
